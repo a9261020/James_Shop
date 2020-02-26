1e65c76f19ac65aac1d7d5d61129fa26a43cc423
James <a9261020@gmail.com>
2020-02-26 14:55:57 +0800

    帳單管理ok
    帳單管理ok

--- src/views/Admin/OrdersManage.vue
+++ src/views/Admin/OrdersManage.vue
@@ -4,34 +4,33 @@
       <table class="table nowrap">
         <thead>
           <tr>
-            <th class="text-center">購買時間</th>
-            <th class="d-sm-table-cell d-none text-center">Email</th>
-            <th class="d-lg-table-cell d-none text-center">購買款項</th>
-            <th class="text-center">應付金額</th>
-            <th class="d-sm-table-cell d-none text-center">是否付款</th>
+            <th>購買時間</th>
+            <th class="d-sm-table-cell d-none ">Email</th>
+            <th class="d-lg-table-cell d-none ">購買款項</th>
+            <th class="text-right">應付金額</th>
+            <th class="d-sm-table-cell d-none ">是否付款</th>
           </tr>
         </thead>
         <tbody>
           <tr
             :class="{ 'bg-light': order.is_paid }"
             v-for="order in orders"
-            :key="order.id"
+            :key="order._id"
           >
-            <td class="text-center">{{ order.createDate }}</td>
+            <td>{{ order.createDate }}</td>
             <td class="d-sm-table-cell d-none" v-if="order.user">
               {{ order.user.email }}
             </td>
 
             <td class="d-lg-table-cell d-none">
               <ul class="list-unstyled">
-                <li v-for="(item, index) in orders" :key="index">
-                  {{ item.products.title }}:{{ item.products.qty
-                  }}{{ item.products.unit }}
+                <li v-for="(item, index) in order.products.carts" :key="index">
+                  {{ item.product.title }}:{{ item.qty }}{{ item.product.unit }}
                 </li>
               </ul>
             </td>
-            <td class="text-right">{{ order.total }}</td>
-            <td class="d-sm-table-cell d-none text-center">
+            <td class="text-right">{{ order.price }}</td>
+            <td class="d-sm-table-cell d-none ">
               <strong class="text-success" v-if="order.is_paid">已付款</strong>
               <span class="text-muted" v-else>尚未付款</span>
             </td>
@@ -57,6 +56,9 @@ export default {
       axios.get(url).then(res => {
         if (res.data.success) {
           this.orders = res.data.orders;
+          this.orders.forEach(item => {
+            item.products = JSON.parse(item.products);
+          });
         }
       });
     }
